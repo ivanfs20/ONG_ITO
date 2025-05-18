a48e1bc598746989cccbbd4e0d48a5eadffde605
Lima811 <saullima811@gmail.com>
2025-05-17 21:11:58 -0600

    BACKEND->model/Proyecto.php(Se agrego el metodo readAll())

--- model/Proyecto.php
+++ model/Proyecto.php
@@ -155,5 +155,32 @@ class Proyecto{
         }
         return $oProyecto;
     }
+
+    //B-PROYECTOS(CAMPAÑAS)->READ ALL : Saul Lima Gonzalez
+    public function readAll(){
+        $oAccesoDatos=new AccesoDatos();
+        $sQuery="";
+        $arrRS=0;
+        $oProyecto=null;
+        if($oAccesoDatos->conectar()){
+            $sQuery="SELECT * FROM Proyecto";
+            $arrRS=$oAccesoDatos->consulta($sQuery);
+            $oAccesoDatos->desconectar();
+            if($arrRS && count($arrRS)>0){
+                $aLinea=$arrRS[0];
+                $oProyecto=new Proyecto();
+                $oProyecto->nIdProyecto=$aLinea[0];
+                $oProyecto->sTitle=$aLinea[1];
+                $oProyecto->sDescription=$aLinea[2];
+                $oProyecto->aPhoto=$aLinea[3];
+                $oProyecto->nIdUsuario=$aLinea[4];
+                $oProyecto->nIdBenefactor=$aLinea[5];
+            };
+        }
+        return $oProyecto;
+    }
+
+       
 }
+
 ?>
