53406ba86d92286ab6dcd094d00ca0afb7f34192
Edwin Ariel Ramos Álvarez <edwin.ramos.alvarez03@gmail.com>
2025-05-17 19:00:36 -0600

    FRONT-EDN>view/js/script1.js|se añadio una funcion para poder manejar las areas seleccionadas en D1_Donar.php>..FRONT-END>view/D1_Area.php|Se añadio vosta del primer paso para poder donar, en este caso solo se selecciona el area correspondiente>..FRONT-END>view/css/vistas/D1_Area.css|Se añadio un estilo para la vista que se creo>..FRONT-END>view/D2_Donar.php|Se añadio una vista vacia para que funcione el redireccionamiento

--- view/modules/navbar.php
+++ view/modules/navbar.php
@@ -11,7 +11,7 @@
         <a href="campanas.php" class="nav-link">Campañas</a>
         <a href="areasapoyo.php" class="nav-link">Áreas de trabajo</a>
         <a href="transparencia.php" class="nav-link">Transparencia</a>
-        <a href="donar.php" class="nav-link">Donar ahora</a>
+        <a href="D1_Area.php" class="nav-link">Donar ahora</a>
         <a href="iniciarsesion.php" class="nav-link login-link">Login</a>
     </div>
 </nav>
