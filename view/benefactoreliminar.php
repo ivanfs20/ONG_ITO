fd064175a151848e6bb3b3111d14f4b69680ee29
UrielVallejoX <urielvx@gmail.com>
2025-05-18 08:18:58 -0600

    FRONT-END > VIEW /benefactoreliminar.php | eliminar registro de la tabla benefactores

---
+++ view/benefactoreliminar.php
@@ -0,0 +1,43 @@
+<?php
+/*************************************************************/
+/* Archivo:  benefactoreliminar.php
+ * Objetivo: Eliminar campo de un benefactor
+ * Autor: Uriel Vallejo Xicalhua
+ *************************************************************/
+$customStyles = '<link rel="stylesheet" href="../view/css/vistas/benefactoreliminar.css">'; #cargamos el estilo en especifico de benefactoreliminar.php
+include_once("modules/header.html");  # Incluye <head> y apertura de <body>
+include_once("modules/navbar.php");   # Navbar
+
+?>
+
+
+<div class="header">Eliminar Benefactor</div>
+
+<div class="container">
+    <table>
+        <tr>
+            <th>Id Proyecto</th>
+            <th>Titulo</th>
+            <th>Descripción</th>
+        </tr>
+    </table>
+
+    <form>
+        <label for="id_proyecto">Id Proyecto:</label>
+        <input type="text" id="id_proyecto" name="id_proyecto">
+
+        <label for="id_titulo">Titulo:</label>
+        <input type="text"  id=" id_titulo " name="id_titulo">
+
+        <label for="id_dscripcion">Descripcion:</label>
+        <input type="text"  id=" id_titulo " name="id_descripcion">
+   <div>
+        <button class="button">Confirmar</button>
+
+</div>
+       
+
+
+<?php
+include_once("modules/footer.html"); # Footer y cierre de HTML
+?>
