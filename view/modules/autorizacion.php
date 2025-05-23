21166f10f45bb5dd90ddb014ed3a4fcb2886b487
Edwin Ariel Ramos Álvarez <edwin.ramos.alvarez03@gmail.com>
2025-05-22 23:39:33 -0600

    FRONT-END>view/modules/autorizacion.php|Se añadio un nuevo componente reutilizable, el cual cuenta con un titulo, subtitulo y texto aclarativo al igual que de una imagen y de un boton para volver, esta tiene funcion en dado caso que un usuario normal quiera sobre pasar sus funcionalidades>..FRONT-END>view/css/componentes/autorizacion.css|Se añadio el clasico diseño de adverencia bien triste>..FRONT-END>view/media/advertencia.png|Simple imagen alusiva

---
+++ view/modules/autorizacion.php
@@ -0,0 +1,16 @@
+<!--  Autor: Edwin Ariel Ramos Alvarez  -->
+<!-- Componente que srive para dar advertencia dado que no cuenta con los permisos
+    correspondientes para poder ingresar a funcionalidades de un admin -->
+
+<section class="seccion-autorizacion">
+    <div class="contenedor-autorizacion">
+        <h2 class="seccion-titulo">Acceso denegado</h2>
+        <h3 class="seccion-subtitulo">Actualmente no tiene permisos de acceso a esta seccion</h3>
+        <p class="texto-aclarativo">Por motivos de seguridad, Donativos ITOrizaba no permite que usuarios adquieran
+            privilegios de administrador</p>
+        <div class="imagen-advertencia">
+            <img src="./media/advertencia.png" alt="imagen de advertencia">
+        </div>
+        <button class="boton-volver" onclick="window.history.back()">Volver atrás</button>
+    </div>
+</section>
