31414bb459ab249108c7193e74168ce75975e996
UrielVallejoX <urielvx@gmail.com>
2025-05-26 12:46:39 -0600

    FRONT-END > VIEW /gestiondetestimonio.php | se modifico totalmente las tablas de indicios por testimonios

---
+++ view/testimoniomodificar.php
@@ -0,0 +1,49 @@
+<?php
+/*************************************************************/
+/* Archivo:  testimoniomodificar.php
+ * Objetivo: Modificar campo de un indiciomodificar
+ * Autor: Uriel Vallejo Xicalhua
+ *************************************************************/
+$customStyles = '<link rel="stylesheet" href="../view/css/vistas/testimoniomodificar.css">'; #cargamos el estilo en especifico de testimoniomodificar.php
+$customScript = '<script src="../view/js/script1.js"></script>'; #cargamos el script
+include_once("modules/header.html");  # Incluye <head> y apertura de <body>
+include_once("modules/navbar.php");   # Navbar
+
+?>
+
+
+<div class="header">Modificar Testimonio</div>
+
+<div class="container">
+    <table>
+        <tr>
+        <th>Id testimonio</th>
+                    <th>Testimonio</th>
+                    <th>Nombre</th>
+                    <th>Carrera</th>
+        </tr>
+    </table>
+
+    <form>
+             
+    <label for="id_testimonio">Id Testimonio:</label>
+        <input name="id_testimonio" type="text" id="id_testimonio" >
+
+        <label for="testimonio">Testimonio:</label>
+        <input name="testimonio" type="text"  id=" testimonio ">
+
+        <label for="id_nombre">Nombre:</label>
+        <input name="id_nombre" type="text"  id=" id_nombre">
+
+        <label for="id_carrera">Carrera:</label>
+        <input name="id_carrera" type="text"  id=" id_carrera">
+   <div>
+        <button class="button">Confirmar</button>
+
+</div>
+       
+
+
+<?php
+include_once("modules/footer.html"); # Footer y cierre de HTML
+?>
