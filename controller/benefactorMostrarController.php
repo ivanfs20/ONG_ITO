92ef9e1a4a3dc0bdd91583e04fde4f21405d2b9d
Lima811 <saullima811@gmail.com>
2025-05-26 21:00:53 -0600

    BACKEND->controller/benefactorMostrarController (Se agrego controlador para mostrar la informacion de benefactores obteniendolos del modelo)

---
+++ controller/benefactorMostrarController.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../model/Benefactor.php';
+$oBenefactor = new Benefactor();
+$arrBenefactor = $oBenefactor->getAll();
+
+foreach ($arrBenefactor as $benefactor) {
+?>
+
+    <tr>
+        <td><?php echo  $benefactor->getnIdBenefactor();  ?></td>
+        <td><?php echo $benefactor->getsName();   ?></td>
+        <td><?php echo $benefactor->getsDescription();   ?></td>
+        <td><button onclick="window.location.href='benefactormodificar.php?idBenefactor=<?php echo $benefactor->getnIdBenefactor(); ?>'" class="btn-modificar">Modificar</button>
+            <button onclick="window.location.href='benefactoreliminar.php?idBenefactor=<?php echo  $benefactor->getnIdBenefactor(); ?>'" class="btn-eliminar">Eliminar</button>
+        </td>
+    </tr>
+<?php
+}
+?>
