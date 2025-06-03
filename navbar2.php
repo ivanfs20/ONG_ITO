ebf82ee603b68af3b9f1812d205e9afd4b174982
Lima811 <saullima811@gmail.com>
2025-06-02 21:15:54 -0600

    BACKEND->controller/materialIndexDosController y digitalIndexDosController.php(se agregaron los controladores para mostrar los mismos datos que el index normal)

--- navbar2.php
+++ navbar2.php
@@ -41,7 +41,7 @@
     </div>
 
     <div class="nav-links">
-           
+        <a href="index.php" class="nav-link">Inicio</a> 
         <a href="campanas.php" class="nav-link">Campañas</a>   
         <a href="areasapoyo.php" class="nav-link">Áreas de trabajo</a>
          <a href="transparencia.php" class="nav-link">Transparencia</a>    
